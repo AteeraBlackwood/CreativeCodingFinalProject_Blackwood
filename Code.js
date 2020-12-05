38c80f6adc76db861fcb56fd65ab854fcda59f27
AteeraBlackwood <aeb734@nyu.edu>
2020-12-05 17:42:30 -0500

    Changed code
    Code update

--- Code.js
+++ Code.js
@@ -1,71 +1,99 @@
 let heart;
 let field;
 let starttime=0;
-let x=246;
-let y=412;
 let heartbeat;
 let leavesrussle;
 let roar;
 let wind;
-let arrow;
+let rightarrow;
+let downarrow;
+let x=0;//allows size values to be changed
+let y=0;	
+
 var victim;
-var frame
-//var SCENE_W = 3000;
-var times=[30000,40000,50000,60000]
+var canvas
+var SCENE_W = 3000;
+var times=[30000,40000,50000,60000,20000]
+
+//frame.focus();
+
+view_xview = 0;
+view_yview = 0;
+view_wview = 1000;//what the viewer can see
+view_hview = 500;//what the viewer can see
+room_width = 3000;//length of hallway
+room_height = 500;
+
 
 function preload(){
-	heart = loadImage("Realistic Heart.png");
-	field = loadImage("Open Field.jpg");//found here: http://www.itsbeautifulhere.net/the-open-field/
-	arrow = loadImage("Arrow.png");//found here: https://toppng.com/photo/402764
-	heartbeat = loadSound("human-heatbeat-daniel_simon.mp3");
-	leavesrussle = loadSound("Leaves Russle On Tree.mp3");
-	roar = loadSound("Monster Alien Roar Agressive.mp3");
-	wind = loadSound("Wind Quiet Creaks.mp3");
-	victim = createSprite(550, 550, 50, 100);
-	//var victimAnimate= victim.addAnimation('http://127.0.0.1:8887/Images/VictimWalkCut_001.png','http://127.0.0.1:8887/Images/assets/VictimWalkCut_014.png');
-}
+	heart = loadImage("RealisticHeart.png");
+	field = loadImage("OpenField.jpg");//found here: http://www.itsbeautifulhere.net/the-open-field/
+	rightarrow = loadImage("Arrow.png");//found here: https://toppng.com/photo/402764
+	downarrow = loadImage("Arrow-Copy.png");
+	victim = loadImage("Victim.PNG");
+	//(550, 550, 50, 100)
+	}
 
 function setup() {
 	createCanvas(1000, 500);
 	background(163, 28, 28);
+	canvas.width = view_wview;
+	canvas.height = view_hview;
+
 }
 
 function draw() {
 	if(millis()-starttime<=10000){//time limit for this animation
-	heartbeat.play();
-}
-	if (heartbeat.isPlaying){
-	image(heart,377,55,x,y);
-	heart.resize(x*1.5,y*1.5);
+
+	image(heart,377,55,x+246,y+412);
+	heart.resize((x+246)*1.5,(y+412)*1.5);
 		loop();
 	}
 	else{
-		heartbeat.pause();
-		background(150, 150, 150);
+		background(173, 173, 173);//change to hallway
 		strokeWeight(2.5);
 		line(0,450,3000,450);
+		noStroke();
+		fill(145,0,0);
+		ellipse(725,450,150,50);//blood
+		circle(825,470,50);
+		stroke(1);
 		doors();
-		animation(victim,150,300);
-		//preload.victimAnimate()
-		
+		image(rightarrow,860,220,50,75);
+		image(downarrow,725,20,50,75);
+		image(victim,x+100,y+100,369,378);
 	}
-	/*if(millis()-starttime==times[0,1,2,3]){
-		background.filter(BLUR);
-	}*/
-	//}
+	if(keyCode===RIGHT_ARROW){//proceed to next part
+		image(victim, x+100,y+100,369,378);
+		x+=4;
+	}
+	if(keyCode===DOWN_ARROW){//leave building
+		let b=100;
+	for(let i=0; i<650;i++){
+		fill(0, 78, 145);
+		rect(650,b,200,350);
+		b+=500;
+		image(victim, x+100,y+100,369,378);
+		x+=4;
+		if(x>=650){
+		background(field,0,0);
+		image(victim, x,y+100,369,378);	
+		x+=4;
+			
+	}
+	if(millis()-starttime==times[0,1,2,3,4]){
+		victim.filter(BLUR);
+	}
+	}
+	}
 	
 }
 function doors(){
 	let b=100;
 	for(let i=0; i<650;i++){
+		fill(150, 150, 150);
 		rect(650,b,200,350);
 		b+=500;
-	}
-}
-
-function mouseClicked(){
-	if(arrow===true){
-		
 	}
 }
 
